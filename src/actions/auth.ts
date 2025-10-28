0c0c61ce5f0fb3f5fc48d9c03f40d00937fed883
muhammaddzaki123 <muhammaddzaki933@gmail.com>
2025-10-28 11:27:00 +0800

    push

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,41 +1,41 @@
-// import NextAuth from "next-auth";
-// import { PrismaAdapter } from "@next-auth/prisma-adapter";
-// import { PrismaClient } from "@prisma/client";
-// import Credentials from "next-auth/providers/credentials";
-// import bcrypt from "bcryptjs";
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-// export const { handlers, auth, signIn, signOut } = NextAuth({
-//   adapter: PrismaAdapter(prisma),
-//   providers: [
-//     Credentials({
-//       name: "Credentials",
-//       credentials: {
-//         email: { label: "Email", type: "email" },
-//         password: { label: "Password", type: "password" },
-//       },
-//       async authorize(credentials) {
-//         if (!credentials?.email || !credentials.password) {
-//           return null;
-//         }
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          return null;
+        }
 
-//         const admin = await prisma.admin.findUnique({
-//           where: { email: credentials.email as string },
-//         });
+        const admin = await prisma.admin.findUnique({
+          where: { email: credentials.email as string },
+        });
 
-//         if (!admin || !bcrypt.compareSync(credentials.password as string, admin.password)) {
-//           return null;
-//         }
+        if (!admin || !bcrypt.compareSync(credentials.password as string, admin.password)) {
+          return null;
+        }
 
-//         return { id: admin.id, name: admin.name, email: admin.email };
-//       },
-//     }),
-//   ],
-//   session: {
-//     strategy: "jwt",
-//   },
-//   pages: {
-//     signIn: "/login",
-//   },
-// });
+        return { id: admin.id, name: admin.name, email: admin.email };
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+});
